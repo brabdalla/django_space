c75efcb30f8fa860e29743049e7d8ebfc43c5e5b
Bruno Santos Abdalla <brabdalla@gmail.com>
2024-05-20 16:06:54 -0400

    Finalização do modulo de BD Alura

--- setup/settings.py
+++ setup/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "galeria",
+    "galeria.apps.GaleriaConfig",
 ]
 
 MIDDLEWARE = [
@@ -110,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "pt-br"
 
-TIME_ZONE = "America/Sao Paulo"
+TIME_ZONE = "America/Sao_Paulo"
 
 USE_I18N = True
 
@@ -128,6 +128,11 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+# Media
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL = "/media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
